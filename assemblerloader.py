4f3a08224fa48e413d47f92dff675b19a80b5835
Sweep09 <60409087+Sweep09@users.noreply.github.com>
2024-12-20 21:34:22 -0500

    Update assemblerloader.py

--- assemblerloader.py
+++ assemblerloader.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 root.withdraw()
 
 file_path = tk.filedialog.askopenfilename(title="Select assembler text file")
-with open(file_path, 'r') as file:
+with open(file_path, 'r', encoding="UTF-8") as file:
     fix = file.read().replace('\r', '')
     pyperclip.copy(fix)
     print('copied to clipboard')
